c12b4af68110363f61ecef9027e4a7398eb9022e
Alexander Lunegov <alunegov@gmail.com>
2022-01-30 23:29:55 +0500

    chore: update deps

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,10 +10,10 @@ plugins {
 
 dependencies {
     //implementation 'androidx.core:core-ktx:1.3.2'
-    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.compose.ui:ui:1.0.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.compose.ui:ui:1.0.5")
     // Tooling support (Previews, etc.)
     implementation("androidx.compose.ui:ui-tooling-preview:${libs.versions.compose.get()}")
     debugImplementation("androidx.compose.ui:ui-tooling:${libs.versions.compose.get()}")
@@ -25,9 +25,9 @@ dependencies {
     implementation("androidx.compose.material:material-icons-core:${libs.versions.compose.get()}")
     implementation("androidx.compose.material:material-icons-extended:${libs.versions.compose.get()}")
     // Integration with activities
-    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.activity:activity-compose:1.4.0")
     // Integration with ViewModels
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
     // Integration with Navigation
     implementation("androidx.navigation:navigation-compose:2.4.0-alpha04")
     // Accompanist
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-        classpath("com.android.tools.build:gradle:7.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:7.0.4")
         //classpath("com.google.gms:google-services:4.3.9")
         //classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
     }
